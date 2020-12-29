76fd10a6d4eaeda7fa0b26894402154ef04f2b64
Adam Kariv <adam.kariv@gmail.com>
2020-12-29 21:37:07 +0200

    Fixes to outro

--- ui/projects/businessgate/src/app/backend.service.ts
+++ ui/projects/businessgate/src/app/backend.service.ts
@@ -57,8 +57,8 @@ export class BackendService {
           this.record.next(record);
         }
       });
-      this.config.getScript(script_version);
     }
+    this.config.getScript(script_version);
   }
 
   update(item) {
